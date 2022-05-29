fa4781347d758b9c2cbeb0b73a4aef14a1767ebe
Carly Magiera <carlymagiera@Carlys-MacBook-Pro.local>
2022-05-29 13:46:50 -0700

    add starter data

--- earthquake_challenge/static/js/challengeLogic.js
+++ earthquake_challenge/static/js/challengeLogic.js
@@ -30,12 +30,11 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
-let tectonicPlates = new L.LayerGroup();
+
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes,
-  "Tectonic Plates": tectonicPlates
+  "Earthquakes": allEarthquakes
 };
 
 // Then we add a control to the map that will allow the user to change which
